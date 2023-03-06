57714061f049f48ad690fb7cbb26b142f6f8f8a7
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2023-03-05 21:22:16 -0800

    Finalized all Models and adjusted Server.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -8,8 +8,30 @@ ProductTag.init(
   {
     // define columns
     id: { 
-      type: DataTypes.Integer 
-    }
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      PrimaryKey: true, 
+      autoIncrement: true, 
+    }, 
+    product_name: { 
+      type: DataTypes.STRING, 
+      allowNull: false, 
+    }, 
+    price: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      validate: { 
+        isDecimal: true, 
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      defaultValue: "10",
+      validate: { 
+        isNumeric: true,
+      },
+    },
   },
   {
     sequelize,
